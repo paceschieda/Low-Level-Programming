58c772b4c60f6c7c1079457f3443260bafe6747c
paceschieda <38768768+paceschieda@users.noreply.github.com>
2019-01-19 16:16:53 +0100

    Pace_Christine_SWD62B (FINAL)

---
+++ Pace_Christine_SWD62B (FINAL)/client.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#include "linked_list.h"
+
+int main(int argc, char *argv[])
+{
+	if(argc == 4)
+	{
+		for(int i = atoi(argv[1]); i < atoi(argv[2]); i++){
+			int sockfd;
+			struct sockaddr_in serv_addr;
+
+			/* Create a socket */
+			sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+			if (sockfd < 0) {
+				fprintf(stderr, "ERROR: Failed to open socket\n");
+				return 1;
+			}
+
+			/* Initialize socket structure (sockarrd_in) */
+			memset(&serv_addr, 0, sizeof(serv_addr));
+			serv_addr.sin_family = AF_INET;
+			serv_addr.sin_port = htons(i);
+
+			/* Connect to the server */
+			if (connect(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == 0) {
+				printf("\nPort: %d connected\n", i);
+				// ADD PORT TO LIST
+				node * newNode = CreateNode(i);
+				Push(&top, newNode);
+			}
+			
+			close(sockfd);
+		}
+
+		// PRINT LIST
+		int size = ListSize(top);
+		int*list;
+		if (size > 0) {
+			printf("\r\n-----------------------\n");
+			printf("\r\nList of Connected Ports\n");
+			list = ShowList(&top);
+		}
+		else if (size == 0) {
+			printf("\nNo Ports where connected!\n");
+		}
+		printf("\r\n-----------------------\n");
+
+		// SAVE TO OUTPUTFILE.CSV
+		FILE *fp_output = fopen(argv[3], "a");
+		if(fp_output != NULL)
+		{
+			for(int i = 0; list[i] != '\0'; ++i) {
+				fprintf(fp_output, "%d \n", list[i]);
+			}
+			fprintf(fp_output, "\n---------------\n");
+			printf("Processing rows... \n");
+			printf("Finished all file, results in scanner_output.csv \n");
+
+			fclose(fp_output);
+		}
+		else
+		{
+			printf("Failed to open %s \n", argv[3]);
+			return -2;
+		}
+
+
+	}
+	else
+	{
+		printf("\nProvide two arguments as range and an output file\n");
+	}
+
+	return 0;
+}
+
+
+
---
+++ Pace_Christine_SWD62B (FINAL)/honeypot.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+#include <ctype.h>
+
+#define MAX_CONNECTIONS 5
+
+struct args {
+    	char *file;
+	int port;
+};
+
+void* handle_client(void *input){
+	int sockfd;
+	struct sockaddr_in serv_addr, cli_addr;
+	socklen_t clilen = sizeof(cli_addr);
+
+	/* Create a socket */
+	sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sockfd < 0) {
+		fprintf(stderr, "ERROR: Failed to open socket\n");
+		return (void*)1;
+	}
+	
+	/* Initialize socket structure (sockarrd_in) */
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK); //or INADDR_ANY
+	int port = ((struct args*)input)->port;
+	serv_addr.sin_port = htons(port);
+
+	/* Bind the host address */
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		fprintf(stderr, "ERROR: bind() failed\n");
+		fprintf(stderr, "Error code: %s\n", strerror(errno));
+		return (void*)2;
+	}
+
+	/* Start listening for the clients (thread blocks) */
+	if (listen(sockfd, MAX_CONNECTIONS) < 0) {
+		fprintf(stderr, "ERROR: listen() failed\n");
+		fprintf(stderr, "Error code: %s\n", strerror(errno));
+		return (void*)3;
+	}
+
+	/* Accept connection from a client */
+	int count = 0;
+	for(;;){
+		int newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+		if (newsockfd < 0) {
+			fprintf(stderr, "ERROR: accept() failed\n");
+			return (void*)4;
+		}
+		count++;
+		fprintf(stderr, "Connection Accepted\n\n");
+		fprintf(stderr, "Total Number of Connections: %i\n", count);
+
+		time_t result = time(NULL);
+		fprintf(stderr, "\nDate and Time: %s \nPort Number: %i\n ---------------------- \n\n", ctime(&result), port);
+
+		#ifdef FULL
+			char *filename = ((struct args*)input)->file;
+			if(filename == NULL)
+			{
+				filename = "hits.log";
+				printf("Warning: missing <file> command line argument, defaulting to %s \n", filename);
+			}
+			// SAVE TO OUTPUTFILE.CSV
+			FILE *fp_output = fopen(filename, "a");
+			if(fp_output != NULL)
+			{
+				fprintf(fp_output, "\nDate and Time: %s \nPort Number: %i\n\n", ctime(&result), port);
+				fprintf(fp_output, "\n---------------------------------------------------------------------------\n");
+				printf("Processing rows... \n");
+				printf("Finished all file, results in %s \n", filename);
+
+				fclose(fp_output);
+			}
+			else
+			{
+				printf("Failed to open %s \n", filename);
+				return (void*)3;
+			}
+		#endif
+
+		close(newsockfd);
+	}
+}
+
+
+int main(int argc, char *argv[]) {
+	if(argc > 1){
+		int thread_res;
+		pthread_t mythread;
+
+		char *name = "";
+		if (isalpha(argv[argc-1][0]) != 0)
+		{
+			name = argv[argc-1];
+			argc = argc-1;
+		}
+
+		for(int i = 1; i < argc; i++)
+		{
+			struct args *passArguments = (struct args *)malloc(sizeof(struct args));
+			passArguments->port = atoi(argv[i]);
+			
+			if((strcmp(name,"") != 0))
+			{
+				passArguments->file = name;
+			}
+
+			thread_res = pthread_create(&mythread, NULL, handle_client, passArguments);
+			if (thread_res != 0)
+			{
+				fprintf(stderr, "ERROR: pthread_create() failed\n");
+				return 5;
+			}
+		}
+		pthread_join(mythread, NULL);
+		
+	}
+	else{
+		// No arguments
+		printf("\nError: No arguments available\n");
+	}
+
+	return EXIT_SUCCESS;
+}
